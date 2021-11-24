65fd7d3258ad14a311fd07a870dca8745c3992eb
Nata-Mendes <natadelimamendes@gmail.com>
2021-11-23 23:52:20 -0300

    renderizar novs

---
+++ modulo-atual/rend-condicional-form/app-ne/src/components/etapa3.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ETAPA 3 - INFORMAÇÕES GERAIS DE ENSINO</h1>
+    <ol>5. Por que você não terminou um curso de graduação?</ol>
+    <input></input>
+    <ol>6. Você fez algum curso complementar?</ol>
+    <input></input>
+    </div>
+  );
+}
+
+export default App;
