91292d0c74ec468f6b19ec6f4407b21e9c4aa08d
Nata-Mendes <natadelimamendes@gmail.com>
2021-12-17 23:36:27 -0300

    treinando

---
+++ modulo-atual/projeto-labefy/labefy-neitan/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import  title from "./styles";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <title>Labefy</title>
+    </div>
+  );
+}
+
+export default App;
