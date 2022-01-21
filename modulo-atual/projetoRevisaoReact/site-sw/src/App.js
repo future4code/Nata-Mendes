3bbcf9e1b4fb52a14d481e6dba242f273b47a9cd
Nata-Mendes <natadelimamendes@gmail.com>
2022-01-20 23:46:03 -0300

    revisao

---
+++ modulo-atual/projetoRevisaoReact/site-sw/src/App.js
@@ -0,0 +1,23 @@
+
+import React from "react";
+import styled from 'styled-components';
+import AppContainer from "./CharacterListPage/styles";
+import React, {useState} from "react";
+
+const setCurrentPage = (props) => {
+const currentPage [list, setList ] = useState ([])
+
+}
+
+
+function App() {
+  return (
+    <AppContainer >
+    <h1> Star wars</h1>
+    <body> evellyn salinha the lima</body>
+     
+    </AppContainer>
+  );
+}
+
+export default App;   
