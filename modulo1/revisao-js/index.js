6d6cc3eae95b7b558777ee3e8a5a89e6cdddf871
EvyLuanNatã <evellypsales@gmail.com>
2022-02-17 23:25:39 -0300

    exercícios

---
+++ modulo1/revisao-js/index.js
@@ -0,0 +1,61 @@
+// const num1 = Number(prompt("insira um número, Zé"))
+//  const num2 = Number(prompt("insira um número, Caposka"))
+
+//  console.log(`aí Mateux, seus números são: num1 = ${num1} e num2 = ${num2}`)
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+// console.log(num1 % num2)
+
+// Exercício 3
+
+// const numeroSorteadoParsa = Math.floor(Math.random() * 10) + 1
+// const numeroQueVaiDa = Number(prompt(`Coloca aí,fela. De um a Deis kkkk`))
+
+// console.log(`o número sorteado foi ${numeroSorteadoParsa}` )
+// if (numeroQueVaiDa === numeroSorteadoParsa) {
+// console.log ("seu número é igual ao numero sorteado, seu filho da puta")
+// } else if (numeroQueVaiDa > numeroSorteadoParsa) {
+// console.log ("é maior né jdhow!?") 
+// } else 
+//     console.log ("menózin")
+
+// Exercício 4 
+
+// const digiteIdade = prompt(`digite sua idade!`)
+// const digiteAltura = prompt(`digite sua altura em centímetros!`)
+// const eDoente = prompt(`tem problema cardíaco? S/N`)
+
+// if (digiteIdade >= 18 && digiteAltura >= 160 && eDoente === "N" || "n") {
+//     console.log("pode entrar, bebê")
+// } else {
+//    console.log ("senta lá, Cláudia!")
+// }
+
+// Exercício 5
+
+// const frase = " Hoje eu vou comer cenoura, ebaa "
+// console.log (frase, frase.length)
+
+// console.log (frase.trim(),  frase.length)
+// console.log (frase.toLowerCase())
+// console.log (frase.includes("comer") )
+// console.log (frase.includes("potatoes") )
+
+// const novaFrase = frase.replaceAll("cenoura" , "batata") 
+// console.log (novaFrase)
+// console.log(novaFrase.includes("comer"))
+// console.log(novaFrase.includes("batata"))
+
+// Exercício 6
+
+const lista = ["batata", "cenoura","beterraba"]
+
+console.log (lista ["1"])
+console.log(lista.length)
+lista.push("mandioca")
+console.log(lista)
+console.log(lista.includes("cenoura"))
+lista.splice(1,1) 
+console.log(lista)
