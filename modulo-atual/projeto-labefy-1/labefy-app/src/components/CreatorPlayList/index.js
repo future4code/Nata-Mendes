6c5eae73c7872f4c6c8da143d847ed4028ae0999
EvyLuanNatã <natadelimamendes@gmail.com>
2022-05-17 01:53:35 -0300

    salvando segunda

--- modulo-atual/projeto-labefy-1/labefy-app/src/components/CreatorPlayList/index.js
+++ modulo-atual/projeto-labefy-1/labefy-app/src/components/CreatorPlayList/index.js
@@ -2,15 +2,36 @@ import React from "react";
 import styled from "styled-components";
 
 const CreatorPlaylist = styled.div`
-
+height: 500px;
+display: flex;
+align-items: center;
+background-color: goldenrod;
 `
 
-const CreatorPlaylistForm = () => {
-   return ( 
+const FormsPlaylist = styled.form`
+display:flex;
+align-items: center;
+background-color: goldenrod;
+`
+
+
+class CreatorPlaylistForm extends ReactComponent  {
+   state = {
+       
+   }
+   
+   render () {
+
+    return ( 
    <CreatorPlaylist>
-    PlaylistCreatorForm
+   <FormsPlaylist>
+       <label> Cadastrar nova Playlist</label>
+       <input placeholder="Nome da Playlist" />
+       <button>Cadastrar Nova Playlist</button>
+   </FormsPlaylist>
     </CreatorPlaylist>
    )
+    };
 }
 
 export default CreatorPlaylistForm
